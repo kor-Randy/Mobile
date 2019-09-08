adc4de3ab2cf12c8f869205a146eae3d03eab5b5
wlgusdn <randy_95@naver.com>
2019-09-09 08:15:55 +0900

    Create EditText in Login

--- app/src/main/java/com/example/wlgusdn/mobile/LoginActivity.kt
+++ app/src/main/java/com/example/wlgusdn/mobile/LoginActivity.kt
@@ -1,2 +1,25 @@
 package com.example.wlgusdn.mobile
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+
+
+class LoginActivity : AppCompatActivity()
+{
+
+    var Password : EditText?= null
+    var Id : EditText? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        Password=findViewById(R.id.Login_Edit_Password)
+        Id = findViewById(R.id.Login_Edit_Id)
+
+
+    }
+
+}
