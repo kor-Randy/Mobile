f8ab66c843545737eec20e932065870f65ba52c5
wlgusdn <randy_95@naver.com>
2019-09-25 14:03:45 +0900

    return

--- app/src/main/java/com/example/wlgusdn/mobile/LoginActivity.kt
+++ app/src/main/java/com/example/wlgusdn/mobile/LoginActivity.kt
@@ -1,7 +1,6 @@
 package com.example.wlgusdn.mobile
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
