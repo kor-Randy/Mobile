502316e7f13e235474c029f1a9485d856f13e586
yrl09 <yrl0917@naver.com>
2019-11-20 17:31:09 +0900

    calendar 날짜 누르면 해당 년,월,일 출력

--- app/src/main/java/com/example/wlgusdn/mobile/MainActivity.kt
+++ app/src/main/java/com/example/wlgusdn/mobile/MainActivity.kt
@@ -10,7 +10,9 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.TextView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -34,7 +36,8 @@ class MainActivity(context : Context) : Fragment(){
     private lateinit var promiseroombtn : Button
     private lateinit var promiselistbtn : Button
     private lateinit var friendbtn : Button
-
+    private lateinit var calendarview:CalendarView
+    private lateinit var showDate:TextView
     private lateinit var et : EditText
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -51,7 +54,8 @@ class MainActivity(context : Context) : Fragment(){
         Promisebtn = view.findViewById(R.id.Main_Button_Promise)
         friendbtn = view.findViewById(R.id.Main_Friend)
         et = view.findViewById(R.id.Main_et)
-
+        calendarview = view.findViewById(R.id.calendarView2)
+        showDate = view.findViewById(R.id.today)
         friendbtn.setOnClickListener(object: View.OnClickListener {
             override fun onClick(v: View?) {
 
@@ -74,9 +78,16 @@ class MainActivity(context : Context) : Fragment(){
             }
         })
 
+        calendarview.setOnDateChangeListener(object:CalendarView.OnDateChangeListener{
+            override fun onSelectedDayChange(p0: CalendarView, p1: Int, p2: Int, p3: Int) {
+                var p2 = p2 + 1
+                var date : String = "${p1}년 ${p2}월 ${p3}일"
+                showDate.text = date
+                Log.d("TAG","onSelectedDayChange:" + date)
 
-
-
+                //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
         return view
 
     }
